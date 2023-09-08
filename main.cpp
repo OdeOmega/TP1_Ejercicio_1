681299ce6f3eeffa36aaf24a817d13851c8db31f
OdeOmega <agustinhermosillabaez@gmail.com>
2023-09-07 22:36:58 -0300

    Add a new project to Git

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
-//lalalalalal
-//lalalalalal
+
 using namespace std;
 
 int calcularPrim(int ult, vector<int> &v,vector<vector<int>> &m2) {
@@ -39,51 +38,53 @@ int calcularUlt(int ult, vector<int> &v,vector<vector<int>> &m2) {
 
 
 int distribuir(int i,int ult, int &N, int K, int &Kinicial, vector<int> &V, vector<vector<vector<int>>> &m,vector<vector<int>> &m2,vector<int> &ubs){
-if(Kinicial == 1){
-int temp1 = calcularPrim(N/2,V,m2);
-int temp2 = calcularUlt(N/2,V,m2);
-return temp1 + temp2;
-}
-else if((N-i)<K) {
-int termin = 10000000;
-return termin;
-}
-else if(K == Kinicial){
-int temp1 = calcularPrim(i,V,m2);
-int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,ubs);
-int ponerla = temp1 + distribuir(i+1,i,N,K-1,Kinicial,V,m,m2,ubs);
-return min(ponerla,noPonerla);
-}
-else if(K == 1 && i < N){
-int temp1 = calcularUlt(i,V,m2);
-if(m2[ult][i] == -1){
-calcular(ult,i,V,m2);
-}
-int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,ubs);
-int ponerla = m2[ult][i] + temp1;
-if(m[i][K][ult] != -1){
-m[i][K][ult] = min(min(ponerla,noPonerla),m[i][K][ult]);
-}
-if(m[i][K][ult] == -1){
-m[i][K][ult] = min(ponerla,noPonerla);
-}
-return m[i][K][ult];
-}
-
-else if(i < N && K > 0) {
-if(m2[ult][i] == -1){
-calcular(ult,i,V,m2);
-}
-int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,ubs);
-int ponerla = m2[ult][i] + distribuir(i+1,i,N,K-1,Kinicial,V,m,m2,ubs);
-if(m[i][K][ult] != -1){
-m[i][K][ult] = min(min(ponerla,noPonerla),m[i][K][ult]);
-}
-if(m[i][K][ult] == -1){
-m[i][K][ult] = min(ponerla,noPonerla);
-}
-return m[i][K][ult];
-}
+    if(Kinicial == 1){
+        int temp1 = calcularPrim(N/2,V,m2);
+        int temp2 = calcularUlt(N/2,V,m2);
+        return temp1 + temp2;
+    }
+    else if((N-i)<K) {
+        //int a = std::numeric_limits<int>::max();
+        int termin = 10000000;
+        return termin;
+    }
+    else if(K == Kinicial){
+        int temp1 = calcularPrim(i,V,m2);
+        int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,ubs);
+        int ponerla = temp1 + distribuir(i+1,i,N,K-1,Kinicial,V,m,m2,ubs);
+        ubs[0] = V[i];
+        return min(ponerla,noPonerla);
+    }
+    else if(K == 1 && i < N){
+        int temp1 = calcularUlt(i,V,m2);
+        if(m2[ult][i] == -1){
+            calcular(ult,i,V,m2);
+        }
+        int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,ubs);
+        int ponerla = m2[ult][i] + temp1;
+        if(m[i][K][ult] != -1){
+            m[i][K][ult] = min(min(ponerla,noPonerla),m[i][K][ult]);
+        }
+        if(m[i][K][ult] == -1){
+            m[i][K][ult] = min(ponerla,noPonerla);
+        }
+        return m[i][K][ult];
+    }
+    else if(i < N && K > 0) {
+        if(m2[ult][i] == -1){
+            calcular(ult,i,V,m2);
+        }
+        int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,ubs);
+        int ponerla = m2[ult][i] + distribuir(i+1,i,N,K-1,Kinicial,V,m,m2,ubs);
+        if(m[i][K][ult] != -1){
+            m[i][K][ult] = min(min(ponerla,noPonerla),m[i][K][ult]);
+        }
+        if(m[i][K][ult] == -1){
+            m[i][K][ult] = min(ponerla,noPonerla);
+        }
+        ubs[i] = V[i];
+        return m[i][K][ult];
+        }
 }
 
 
@@ -91,15 +92,13 @@ int main(){
     int N;
     int K;
     vector<int> V;
-    N = 8;
-    K = 6;
-    V = {4,38,45,46,209,300,490,500};
+    N = 5;
+    K = 3;
+    V = {1,5,15,20,27};
     vector<vector<vector<int>>> memoria(N, vector<vector<int>>(K, vector<int>(N, -1)));
     vector<vector<int>> memoria2(N, vector<int>(N,-1));
 
     vector<int> ubics(K,-1);
-
-    int ult = -1;
     int res = distribuir(0,0,N,K,K,V,memoria,memoria2,ubics);
 
     return 0;
