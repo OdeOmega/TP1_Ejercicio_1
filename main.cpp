05c2e373d1d33ef134628ea8e4b5dd3d70361946
OdeOmega <agustinhermosillabaez@gmail.com>
2023-09-09 12:59:20 -0300

    Add a new project to Git

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <vector>
 
+enum decision {poner,noPoner,UNDEF};
 using namespace std;
+
+vector<int> armarSol(vector<vector<vector<decision>>> &m3, vector<int> &v, int K){
+    int ult = 0;
+    vector<int> res = {};
+    for(int i = 0; i < v.size();i++){
+        if(m3[i][K][ult] == poner){
+            K = K-1;
+            ult = i;
+            res.push_back(v[i]);
+        }
+    }
+    return res;
+}
 
 int calcularPrim(int ult, vector<int> &v,vector<vector<int>> &m2) {
     int res = 0;
@@ -37,22 +51,27 @@ int calcularUlt(int ult, vector<int> &v,vector<vector<int>> &m2) {
 
 
 
-int distribuir(int i,int ult, int &N, int K, int &Kinicial, vector<int> &V, vector<vector<vector<int>>> &m,vector<vector<int>> &m2,vector<int> &ubs){
+int distribuir(int i,int ult, int &N, int K, int &Kinicial, vector<int> &V, vector<vector<vector<int>>> &m,vector<vector<int>> &m2,vector<vector<vector<decision>>> &m3){
     if(Kinicial == 1){
         int temp1 = calcularPrim(N/2,V,m2);
         int temp2 = calcularUlt(N/2,V,m2);
+        m3[i][K][ult] = poner;
         return temp1 + temp2;
     }
     else if((N-i)<K) {
-        //int a = std::numeric_limits<int>::max();
         int termin = 10000000;
         return termin;
     }
     else if(K == Kinicial){
         int temp1 = calcularPrim(i,V,m2);
-        int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,ubs);
-        int ponerla = temp1 + distribuir(i+1,i,N,K-1,Kinicial,V,m,m2,ubs);
-        ubs[0] = V[i];
+        int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,m3);
+        int ponerla = temp1 + distribuir(i+1,i,N,K-1,Kinicial,V,m,m2,m3);
+        if (min(ponerla,noPonerla) == ponerla){
+            m3[i][K][ult] = poner;
+        }
+        if (min(ponerla,noPonerla) == noPonerla){
+            m3[i][K][ult] = noPoner;
+        }
         return min(ponerla,noPonerla);
     }
     else if(K == 1 && i < N){
@@ -60,7 +79,7 @@ int distribuir(int i,int ult, int &N, int K, int &Kinicial, vector<int> &V, vect
         if(m2[ult][i] == -1){
             calcular(ult,i,V,m2);
         }
-        int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,ubs);
+        int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,m3);
         int ponerla = m2[ult][i] + temp1;
         if(m[i][K][ult] != -1){
             m[i][K][ult] = min(min(ponerla,noPonerla),m[i][K][ult]);
@@ -68,21 +87,32 @@ int distribuir(int i,int ult, int &N, int K, int &Kinicial, vector<int> &V, vect
         if(m[i][K][ult] == -1){
             m[i][K][ult] = min(ponerla,noPonerla);
         }
+        if (min(ponerla,noPonerla) == ponerla){
+            m3[i][K][ult] = poner;
+        }
+        if (min(ponerla,noPonerla) == noPonerla){
+            m3[i][K][ult] = noPoner;
+        }
         return m[i][K][ult];
     }
     else if(i < N && K > 0) {
         if(m2[ult][i] == -1){
             calcular(ult,i,V,m2);
         }
-        int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,ubs);
-        int ponerla = m2[ult][i] + distribuir(i+1,i,N,K-1,Kinicial,V,m,m2,ubs);
+        int noPonerla = distribuir(i+1,ult,N,K,Kinicial,V,m,m2,m3);
+        int ponerla = m2[ult][i] + distribuir(i+1,i,N,K-1,Kinicial,V,m,m2,m3);
         if(m[i][K][ult] != -1){
             m[i][K][ult] = min(min(ponerla,noPonerla),m[i][K][ult]);
         }
         if(m[i][K][ult] == -1){
             m[i][K][ult] = min(ponerla,noPonerla);
         }
-        ubs[i] = V[i];
+        if (min(ponerla,noPonerla) == ponerla){
+            m3[i][K][ult] = poner;
+        }
+        if (min(ponerla,noPonerla) == noPonerla){
+            m3[i][K][ult] = noPoner;
+        }
         return m[i][K][ult];
         }
 }
@@ -92,14 +122,14 @@ int main(){
     int N;
     int K;
     vector<int> V;
-    N = 5;
-    K = 3;
-    V = {1,5,15,20,27};
-    vector<vector<vector<int>>> memoria(N, vector<vector<int>>(K, vector<int>(N, -1)));
+    N = 4;
+    K = 2;
+    V = {1,5,15,20};
+    vector<vector<vector<int>>> memoria(N+1, vector<vector<int>>(K+1, vector<int>(N+1, -1)));
     vector<vector<int>> memoria2(N, vector<int>(N,-1));
+    vector<vector<vector<decision>>> memoria3(N+1, vector<vector<decision>>(K+1, vector<decision>(N+1, UNDEF)));
 
-    vector<int> ubics(K,-1);
-    int res = distribuir(0,0,N,K,K,V,memoria,memoria2,ubics);
-
+    int res = distribuir(0,0,N,K,K,V,memoria,memoria2,memoria3);
+    vector<int> ubics = armarSol(memoria3,V,K);
     return 0;
 }
